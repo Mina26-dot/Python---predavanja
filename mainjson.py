61670a6643b8a0be8a4fa156fca7aef5bbbe306f
Mina26-dot <mina.vasiljevic.brains24@gmail.com>
2024-11-18 08:03:07 +0100

    *Renamed data.json to user.json *Added products.json without any data *Changed references in main.py

--- mainjson.py
+++ mainjson.py
@@ -1,7 +1,7 @@
 import json
 from textwrap import indent
 
-with open("data.json", 'r') as file:
+with open("data/user.json", 'r') as file:
     data = json.load(file)
     data.append({
         "name": "Marko",
@@ -10,5 +10,5 @@ with open("data.json", 'r') as file:
 print(data)
 
 
-with open("data.json", 'w') as file:
+with open("data/user.json", 'w') as file:
     json.dump(data, file, indent=4)
