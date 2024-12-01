5feeb4157869f13bd2f3eabd65afa829b6a4857b
Mina26-dot <mina.vasiljevic.brains24@gmail.com>
2024-12-01 07:03:42 +0100

    * Refactor: * Modularize code by separating database connection,   query execution, and CRUD logic

--- database/main.py
+++ database/main.py
@@ -1,29 +1,14 @@
-import mysql.connector
+from users import Users
 
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="kursIT_Obuka",
-        database="python"
-    )
+def main():
+    users = Users("users")
 
-def execute_query(table_name, query, params=None, fetch=False):
-    connection = get_connection()
-    cursor = connection.cursor()
+    # users.insert("Marija", "Arsenijevic", 20)
+    # users.fetch_all()
+    # users.update("Ivana", "Ivic", 56, 20)
+    # users.fetch_all()
+    # users.delete(2)
+    users.fetch_all()
 
-    query = query.format(table_name)
-
-    try:
-        cursor.execute(query, params)
-        if fetch:
-            result = cursor.fetchall()
-            return result
-        else:
-            connection.commit()
-            print("Upit je uspesno izvrsen.")
-    except mysql.connector.Error as err:
-        print(f"Greska u upitu: {err}")
-    finally:
-        cursor.close()
-        connection.close()
+if __name__ == "__main__":
+    main()
