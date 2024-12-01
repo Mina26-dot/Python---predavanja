2c9fab528b36bc33aa8dc9bac9cd3d8056fc2ffb
Mina26-dot <mina.vasiljevic.brains24@gmail.com>
2024-12-01 07:17:12 +0100

    * Refactor connection

--- database/connection.py
+++ database/connection.py
@@ -8,22 +8,3 @@ def get_connection():
         database="python"
     )
 
-def execute_query(table_name, query, params=None, fetch=False):
-    connection = get_connection()
-    cursor = connection.cursor()
-
-    query = query.format(table_name)
-
-    try:
-        cursor.execute(query, params)
-        if fetch:
-            result = cursor.fetchall()
-            return result
-        else:
-            connection.commit()
-            print("Upit je uspesno izvrsen.")
-    except mysql.connector.Error as err:
-        print(f"Greska u upitu: {err}")
-    finally:
-        cursor.close()
-        connection.close()
